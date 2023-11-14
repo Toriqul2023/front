ceb491714bc27ac11a3938c01bf4bca3c11551df
Toriqul2023 <toriqulislam296@yahoo.com>
2023-11-14 19:34:27 +0600

    navbar

--- src/app/Components/Navbar/Navbar.jsx
+++ src/app/Components/Navbar/Navbar.jsx
@@ -13,12 +13,13 @@ const Navbar = () => {
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[999] text-black p-2 shadow bg-base-100 rounded-box w-52">
         
         <li>
           <Link href='https://www.facebook.com/mohammad.toriqul.toriq'>Parent</Link>
           <ul className="p-2">
             <li><Link href='https://www.facebook.com/mohammad.toriqul.toriq'>Submenu 1</Link></li>
+            <li><Link href='https://www.facebook.com/mohammad.toriqul.toriq'>Submenu 2</Link></li>
           
           </ul>
         </li>
