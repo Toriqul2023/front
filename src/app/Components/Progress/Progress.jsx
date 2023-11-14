51e4e87f1ab9f76f370ac13e7a8d5f872080d0c1
Toriqul2023 <toriqulislam296@yahoo.com>
2023-11-14 21:12:33 +0600

    swiper eidt

--- src/app/Components/Progress/Progress.jsx
+++ src/app/Components/Progress/Progress.jsx
@@ -12,9 +12,11 @@ const Progress = () => {
     <div className='bg-[#0B0D0E]'>
        <div className="container mx-auto py-4 ">
         <div className="row ">
-          <div className='p-5'>
+          <div className='p-5 '>
           <Swiper
+         
         slidesPerView={4}
+        centeredSlides={false}
         breakpoints={{
           // when window width is >= 640px
           300: {
@@ -25,36 +27,36 @@ const Progress = () => {
          
         }}
       
-        spaceBetween={30}
+        
         pagination={{
           clickable: true,
         }}
         modules={[Pagination]}
-        className=" grid grid-cols-4 gap-3 justify-center"
+        className="   justify-center"
       >
-          <SwiperSlide>
-         <div>
-          <div className="my-4 radial-progress" id="alb1" aria-label="aib1" name='progress-1' style={{ "--value": "25", "--size": "10rem", "--thickness": "10px","color":"#08D665" }} role="progressbar">
+          <SwiperSlide className='place-self-center'>
+         <div className='place-self-center'>
+          <div className="my-4 mx-auto radial-progress" id="alb1" aria-label="aib1" name='progress-1' style={{ "--value": "25", "--size": "10rem", "--thickness": "10px","color":"#08D665" }} role="progressbar">
             <span className='text-4xl text-white'>65%</span></div>
           </div>
 
-         </SwiperSlide>
-         <SwiperSlide>
-         <div>
+         </SwiperSlide >
+         <SwiperSlide className='place-self-center'>
+         <div className='place-self-center'>
           <div className="my-4 radial-progress" id="alb2" aria-label="aib2" name='progress-2' style={{ "--value": "65", "--size": "10rem", "--thickness": "10px","color":"#08D665" }} role="progressbar">
             <span className='text-4xl text-white'>65%</span></div>
           </div>
 
          </SwiperSlide>
-         <SwiperSlide>
-         <div>
+         <SwiperSlide className='place-self-center'>
+         <div className='place-self-center'>
           <div className="my-4 radial-progress"  id="alb3" aria-label="aib3" name='progress-3' style={{ "--value": "80", "--size": "10rem", "--thickness": "10px","color":"#08D665" }} role="progressbar">
             <span className='text-4xl text-white'>80%</span></div>
           </div>
          </SwiperSlide>
 
-         <SwiperSlide> 
-         <div>
+         <SwiperSlide className='place-self-center'> 
+         <div className='place-self-center'>
           <div 
           className="my-4 radial-progress" id="alb4" aria-label="aib4" name='progress-4' 
           style={{ "--value": "60", "--size": "10rem", "--thickness": "10px","color":"#08D665" }} role="progressbar">
